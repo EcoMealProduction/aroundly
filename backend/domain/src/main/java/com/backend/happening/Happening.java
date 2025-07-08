5939c26984ac1b57b3cfc530617cf077db98d0d0
Calin <calin.ganta2003@gmail.com>
2025-07-08 18:19:33 +0300

    domain Happening implementation

--- backend/domain/src/main/java/com/backend/happening/Happening.java
+++ backend/domain/src/main/java/com/backend/happening/Happening.java
@@ -1,4 +1,17 @@
 package com.backend.happening;
 
+import com.backend.shared.Location;
+import com.backend.user.Comment;
+import com.backend.user.User;
+
+import java.util.List;
+
 public interface Happening {
+    String title();
+    String description();
+    User user();
+    Location location();
+    List<Comment> comments();
+    int likes();
+    int dislikes();
 }
