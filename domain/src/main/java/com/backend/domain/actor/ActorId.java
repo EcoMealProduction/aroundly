4bd744ebb6e3ac8431e1563d8aa585106dccaefa
DanCh11 <dan.chiriac1453@gmail.com>
2025-08-24 13:34:46 +0200

    changed id type to string because keycloak requires

--- domain/src/main/java/com/backend/domain/actor/ActorId.java
+++ domain/src/main/java/com/backend/domain/actor/ActorId.java
@@ -3,6 +3,6 @@ package com.backend.domain.actor;
 /** 
  * Strongly typed domain ID for an Actor (immutable, value-based equality). 
  */
-public record ActorId(long id) {
+public record ActorId(String id) {
     
 }
