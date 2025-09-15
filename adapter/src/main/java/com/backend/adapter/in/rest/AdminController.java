9553ba1e94f1887e333315949be1121a20f8523c
DanCh11 <dan.chiriac1453@gmail.com>
2025-09-15 16:28:00 +0200

    removed "" from request mapping

--- adapter/src/main/java/com/backend/adapter/in/rest/AdminController.java
+++ adapter/src/main/java/com/backend/adapter/in/rest/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
 @Tag(name = "Admin", description = "Administrative endpoints")
 @SecurityRequirement(name = "bearerAuth")
 public class AdminController {
