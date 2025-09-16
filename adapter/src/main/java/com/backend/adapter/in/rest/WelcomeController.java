5e0527591173b9a4533900d2b7080d4377fdd127
DanCh11 <dan.chiriac1453@gmail.com>
2025-09-16 20:39:32 +0200

    health controller

--- adapter/src/main/java/com/backend/adapter/in/rest/WelcomeController.java
+++ adapter/src/main/java/com/backend/adapter/in/rest/WelcomeController.java
@@ -2,15 +2,13 @@ package com.backend.adapter.in.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 @Tag(name = "Welcome", description = "Welcome endpoint to test the deploy")
 public class WelcomeController {
 
-  @GetMapping
+  @GetMapping("healthz")
   public String welcome() {
     return "Welcome";
   }
