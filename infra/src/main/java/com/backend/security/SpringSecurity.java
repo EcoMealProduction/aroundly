30cbd01b5ebe42ae94c4f37967597d3769980931
DanCh11 <dan.chiriac1453@gmail.com>
2025-09-17 21:38:24 +0200

    removed a thing

--- infra/src/main/java/com/backend/security/SpringSecurity.java
+++ infra/src/main/java/com/backend/security/SpringSecurity.java
@@ -40,7 +40,6 @@ public class SpringSecurity {
                 .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/api-docs/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/api/v1/**").authenticated()
                 .requestMatchers("/healthz").permitAll()
-                .anyRequest().authenticated()
             )
             .logout(logout -> logout.logoutSuccessUrl("/auth/login"))
             .oauth2ResourceServer(oauth2 -> oauth2
