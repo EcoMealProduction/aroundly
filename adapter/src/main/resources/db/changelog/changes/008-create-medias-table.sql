5ae6d19e134595fb797d2e671a0418dd287ce1bf
DanCh11 <dan.chiriac1453@gmail.com>
2025-10-04 20:01:26 +0200

    chenged configs

--- adapter/src/main/resources/db/changelog/changes/008-create-medias-table.sql
+++ adapter/src/main/resources/db/changelog/changes/008-create-medias-table.sql
@@ -4,9 +4,9 @@
 CREATE TABLE IF NOT EXISTS medias (
                                          id           BIGINT NOT NULL PRIMARY KEY,
                                          key          VARCHAR(255) NOT NULL,
-                                         contentType  VARCHAR(255),
+                                         content_type  VARCHAR(255),
                                          size BIGINT NOT NULL,
-                                         createdAt TIMESTAMP,
+                                         created_at TIMESTAMP,
                                          address_text VARCHAR(500),
                                          happening_id BIGINT NOT NULL,
                                          CONSTRAINT FK_MEDIA_HAPPENING FOREIGN KEY (happening_id) REFERENCES happenings (id)
